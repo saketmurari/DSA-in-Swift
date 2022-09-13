5d605d5384db83457e61145cb1038e920798c71a
saketmurari <saketmurari@gmail.com>
2022-09-13 23:43:45 +0530

    minimum-falling-sum-all-case

--- Leetcode-120-triangle-all-solution.swift
+++ Leetcode-120-triangle-all-solution.swift
@@ -1,71 +1,148 @@
 class Solution {
-    
-    func minimumTotal(_ triangle: [[Int]]) -> Int {
+    func minFallingPathSum(_ matrix: [[Int]]) -> Int {
         
-        var rows = triangle.count
-        var columns = triangle.last!.count
+       /* below code is for recursion solution to work
+        var rows = matrix.count
+        var columns = matrix[0].count
+        var minimum = Int.max
+        var dp = Array(repeating: Array(repeating: -101, count: columns), count: rows)
         
+        for i in 0...columns-1 {
+             let maxi = findMinimumPathSum(0, i, matrix, &dp)
+             minimum = min(minimum, maxi)
+        }
+        */
         
-        return spaceOptimised(rows, columns, triangle)
+        /* this is for tabulation solution
+              return tabulation(matrix)
+        */
         
+        // This is for best possible case where Time complexity if O(n*m) and space complexity O(1)
+        
+        return optimised(matrix)
+        // return minimum
     }
     
-    func findMinimum( _ triangle: [[Int]], _ i:Int, _ j: Int, _ dp: inout [[Int]]) -> Int {
+    func findMinimumPathSum ( _ i: Int, _ j: Int, _ mat: [[Int]], _ dp: inout [[Int]]) -> Int {
         
-        if i == triangle.count-1 {
-            return triangle[i][j]
+        if j < 0 || j > mat[0].count-1 {
+            return Int.max
         }
-        if dp[i][j] != -100001 {
+        
+        if i == mat.count-1 {
+            return mat[mat.count-1][j]
+        }
+        
+        if dp[i][j] != -101 {
             return dp[i][j]
         }
         
-        var path1Sum = triangle[i][j] + findMinimum(triangle, i+1, j, &dp)
-        var path2Sum = triangle[i][j] + findMinimum(triangle, i+1, j+1, &dp)
+        var left = Int.max
+        var down = Int.max
+        var right = Int.max
         
+        if j - 1 >= 0 {
+            left = mat[i][j] + findMinimumPathSum(i+1, j-1, mat, &dp)
+        }
         
-        dp[i][j] = min(path1Sum, path2Sum)
+        down = mat[i][j] + findMinimumPathSum(i+1, j, mat, &dp)
+        
+        if j + 1 < mat[0].count {
+            right = mat[i][j] + findMinimumPathSum(i+1, j+1, mat, &dp)
+        }
+        
+        dp[i][j] =  min(left, down, right)
         return dp[i][j]
     }
     
-    
-    func tabulation( _ rows:Int, _ columns:Int, _ triangle: [[Int]]) -> Int {
+    func tabulation(_ matrix: [[Int]] ) -> Int {
         
-      var dp = Array(repeating: Array(repeating: 0, count: columns), count: rows)
-      dp[rows-1] = triangle[rows-1]
+        var rows = matrix.count
+        var columns = matrix[0].count
+        var dp = Array(repeating: Array(repeating: 0, count: columns), count: rows)
+
+        dp[rows-1] = matrix[rows-1]
+        
         var i = rows-2
         
         while i >= 0 {
+            
             var j = 0
-            while j <= i {
-                var left = triangle[i][j] + dp[i+1][j]
-                var right = triangle[i][j] + dp[i+1][j+1]
-                dp[i][j] = min(left, right)
+            while j < columns {
+                
+                var left = Int.max
+                var down = Int.max
+                var right = Int.max
+                
+                if j - 1 >= 0 {
+                    left = matrix[i][j] + dp[i+1][j-1]
+                }
+                
+                down = matrix[i][j] + dp[i+1][j]
+                
+                if j + 1 < columns {
+                    right = matrix[i][j] + dp[i+1][j+1]
+                }
+                dp[i][j] =  min(left, down, right)
                 j += 1
             }
             i -= 1
         }
-        return dp[0][0]
-    }
-    
-    
-    func spaceOptimised( _ rows:Int, _ columns:Int, _ triangle: [[Int]]) -> Int {
         
-      var prev = triangle[rows-1]
-      var i = rows-2
+        var result = Int.max
+        
+        for item in dp.first! {
+            result = min(result, item)
+        }
+        
+        return result
+        
+    }
+    
+    func optimised(_ matrix: [[Int]]) -> Int {
+        
+        var rows = matrix.count
+        var columns = matrix[0].count
+        var dp = Array(repeating: 0, count: columns)
+
+        dp = matrix[rows-1]
+        
+        var i = rows-2
+        
         while i >= 0 {
+            
             var j = 0
-            
-            var current = Array(repeating: 0, count: i+1)
-            
-            while j <= i {
-                var left = triangle[i][j] + prev[j]
-                var right = triangle[i][j] + prev[j+1]
-                current[j] = min(left, right)
+            var current = Array(repeating: 0, count: columns)
+            while j < columns {
+                
+                var left = Int.max
+                var down = Int.max
+                var right = Int.max
+                
+                if j - 1 >= 0 {
+                    left = matrix[i][j] + dp[j-1]
+                }
+                
+                down = matrix[i][j] + dp[j]
+                
+                if j + 1 < columns {
+                    right = matrix[i][j] + dp[j+1]
+                }
+                current[j] =  min(left, down, right)
                 j += 1
             }
-            prev = current
+            dp = current
             i -= 1
         }
-        return prev[0]
+        
+        var result = Int.max
+        
+        for item in dp {
+            result = min(result, item)
+        }
+        
+        return result
+        
     }
+    
 }
